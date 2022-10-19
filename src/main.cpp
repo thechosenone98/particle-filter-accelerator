2ceab0b8af076b14ac3053c7f60c1b729a20c1fb
Zacharie McCormick <zacfire1998@hotmail.fr>
2022-10-20 00:32:13 +0200

    Working accelerator

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,6 @@ public:
         uint8_t obstacle_y[800] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20,21,21,22,22,23,23,24,24,25,25,26,26,26,26,26,26,26,26,26,26,26,26,27,27,27,27,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,36,36,36,36,36,36,36,36,37,37,38,38,39,39,40,40,41,41,41,41,41,41,41,41,41,41,41,41,42,42,42,42,43,43,43,43,44,44,44,44,45,45,45,45,46,46,46,46,47,47,47,47,48,48,48,48,49,49,49,49,50,50,50,50,50,50,50,50,50,50,50,50,51,51,52,52,53,53,54,54,55,55,56,56,57,57,58,58,59,59,60,60,61,61,62,62,63,63,64,64,65,65,66,66,67,67,68,68,69,69,70,70,71,71,72,72,73,73,74,74,75,75,76,76,77,77,78,78,79,79,80,80,81,81,82,82,82,82,82,82,82,82,82,82,82,82,83,83,83,83,84,84,84,84,85,85,85,85,86,86,86,86,86,86,86,86,86,86,86,86,86,86,87,87,87,87,87,87,88,88,88,88,88,88,89,89,89,89,89,89,90,90,90,90,90,90,91,91,91,91,91,91,91,91,91,91,91,91,91,91,92,92,92,92,93,93,93,93,94,94,94,94,95,95,95,95,95,95,95,95,95,95,95,95,96,96,97,97,97,97,97,97,97,97,97,97,97,97,98,98,98,98,99,99,99,99,100,100,100,100,101,101,101,101,102,102,102,102,103,103,103,103,104,104,104,104,105,105,105,105,106,106,106,106,106,106,106,106,106,106,106,106,107,107,108,108,109,109,110,110,111,111,112,112,113,113,114,114,115,115,116,116,117,117,118,118,119,119,120,120,121,121,122,122,123,123,124,124,125,125,126,126,127,127,128,128,129,129,130,130,131,131,132,132,133,133,134,134,135,135,136,136,137,137,138,138,139,139,140,140,141,141,142,142,143,143,144,144,145,145,146,146,147,147,148,148,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149,149, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
         uint8_t nb_scans = 0;
         int32_t min_distance = 0;
-        // uint32_t distance_x1, distance_x2;
-        // uint32_t distance_y1, distance_y2;
-        // uint16_t min_dist1 = 0;
-        // uint16_t min_dist2 = 0;
-        // uint16_t min_dist_final = 0;
-        // uint8_t oxi1 = 0;
-        // uint8_t oyi1 = 0;
-        // uint8_t oxi2 = 0;
-        // uint8_t oyi2 = 0;
-        // uint8_t oxi_final = 0;
-        // uint8_t oyi_final = 0;
-        // uint16_t min_distance_xy_index = 0;
-        // uint16_t min_distance_y_index = 0;
         int16_t particles_x[NUM_PARTICLES*9];
         int16_t particles_y[NUM_PARTICLES*9];
         uint32_t inst;
@@ -60,68 +47,27 @@ public:
                     min_distance = 0x7FFFFFFF;
                     int16_t px = particles_x[particle_index];
                     int16_t py = particles_y[particle_index];
-                    // _PIPELINE_
+                    _PIPELINE_
                     for (i1 = 0; i1 < 400; i1++){
                         int16_t ox1 = obstacle_x[i1];
                         int16_t oy1 = obstacle_y[i1];
                         int16_t ox2 = obstacle_x[i1+400];
                         int16_t oy2 = obstacle_y[i1+400];
-                        // distance_x1 = (int32_t)(px - ox1) * (int32_t)(px - ox1);
-                        // distance_y1 = (int32_t)(py - oy1) * (int32_t)(py - oy1);
-                        // distance_x2 = (int32_t)(px - ox2) * (int32_t)(px - ox2);
-                        // distance_y2 = (int32_t)(py - oy2) * (int32_t)(py - oy2);
-                        // if(distance_x1 + distance_y1 < min_distance){
-                        //     min_distance = distance_x1 + distance_y1;
-                        // }
+
                         int16_t distance_x1 = px - ox1;
                         int16_t distance_y1 = py - oy1;
                         int16_t distance_x2 = px - ox2;
                         int16_t distance_y2 = py - oy2;
 
-                        int32_t distance1 = (int32_t)distance_x1 * (int32_t)distance_x1 + (int32_t)distance_y1 * (int32_t)distance_y1;
-                        int32_t distance2 = (int32_t)distance_x2 * (int32_t)distance_x2 + (int32_t)distance_y2 * (int32_t)distance_y2;
-
-                        // int32_t distance3 = distance_x3 * distance_x3 + distance_y3 * distance_y3;
-                        // uint16_t distance4 = distance_x4 * distance_x4 + distance_y4 * distance_y4;
-                        // if(distance1 < distance2){
-                        //     min_dist1 = distance1;
-                        //     oxi1 = ox1;
-                        //     oyi1 = oy1;
-                        // } else {
-                        //     min_dist1 = distance2;
-                        //     oxi1 = ox2;
-                        //     oyi1 = oy2;
-                        // }
-                        // if (distance3 < distance4){
-                        //     min_dist2 = distance3;
-                        //     oxi2 = ox3;
-                        //     oyi2 = oy3;
-                        // } else {
-                        //     min_dist2 = distance4;
-                        //     oxi2 = ox4;
-                        //     oyi2 = oy4;
-                        // }
-                        // if(min_dist1 < min_dist2){
-                        //     min_dist_final = min_dist1;
-                        //     oxi_final = oxi1;
-                        //     oyi_final = oyi1;
-                        // } else {
-                        //     min_dist_final = min_dist2;
-                        //     oxi_final = oxi2;
-                        //     oyi_final = oyi2;
-                        // }
-                        // min_dist1 = MIN(distance1, distance2);
-                        // min_dist2 = MIN(distance3, distance4);
+                        int32_t distance1 = (int32_t)(distance_x1 * distance_x1) + (int32_t)(distance_y1 * distance_y1);
+                        int32_t distance2 = (int32_t)(distance_x2 * distance_x2) + (int32_t)(distance_y2 * distance_y2);
 
                         int32_t min_dist_min = MIN(distance1, distance2);
                         if (min_dist_min < min_distance){
                             min_distance = min_dist_min;
-                            // min_distance_xy_index = (oxi_final << 8) | oyi_final;
                         }
                     }
-                    // uint16_t dist = isqrt32(min_distance<<16);
                     ostream.write(min_distance);
-                    // ostream.write(min_distance_xy_index);
                 }
             }
         }
